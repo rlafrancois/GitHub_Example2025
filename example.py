f294e07574572dcc23cc34798fd02c26b9e0797d
Adrien Osakwe <adrien.osakwe@mail.mcgill.ca>
2025-06-26 09:58:06 -0400

    Create plotting function for script

--- example.py
+++ example.py
@@ -1,8 +1,14 @@
-
+import numpy as np 
+import matplotlib.pyplot as plt
 x = input("Enter name here: ")
 
 print('hello' + x)
 
-y = int(input"enter: "))
 
-print('year: ' + y)
+y = np.array([i for i in range(20)])
+y2 = 2*y
+
+plt.plot(y,y2)
+plt.savefig('test.png')
+
+
